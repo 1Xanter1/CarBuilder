8445574334eb38e8e55417ef06741b924d852a68
Xanter <ntapproved@gmail.com>
2026-09-08 17:18:52 +0500

    -Made testing classes in the Main

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
-
 public class Main {
+    public static void main(String[] args) {
+        CarDirector carDirector = new CarDirector();
+
+        Car lightweightCar = carDirector.buildLightweightCar(new LightweightCarBuilder());
+
+        Car sportsCar = carDirector.buildSportsCar(new SportscarBuilder());
+
+        Car truck = carDirector.buildTruck(new TruckBuilder());
+
+        System.out.println("Lightweight Car:");
+        System.out.println(lightweightCar);
+
+        System.out.println("\nSports Car:");
+        System.out.println(sportsCar);
+
+        System.out.println("\nTruck:");
+        System.out.println(truck);
+    }
 }
